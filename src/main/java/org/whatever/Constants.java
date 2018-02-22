84125556f512353aa5f9d4d597c89b37f74e0565
Kevin Hutson <Kevin.Hutson@blackbaud.com>
2018-02-22 16:19:40 -0600

    buttons and things

--- src/main/java/org/whatever/Constants.java
+++ src/main/java/org/whatever/Constants.java
@@ -1,7 +1,9 @@
 package org.whatever;
 
 public class Constants {
-    public static final String AUDIO_TOPIC = "TT-AUDIO-2";
+    public static final String AUDIO_TOPIC = "TT-AUDIO-4";
     public static final String LOGO_IMAGE = "kafkaAmp.png";
     public static final String KAFKA_BOOTSTRAP_SERVERS = "local.docker:9092";
+    public static final String LOCAL_AUDIO_STORE_PATH = "/Users/kevin.hutson/dev/techThursday/kafka-udp-streamer/songs";
+
 }
