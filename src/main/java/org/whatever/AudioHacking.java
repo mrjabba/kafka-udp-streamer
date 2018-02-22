13161aba180f58119d25adca94a53c1e87533cdd
Kevin Hutson <Kevin.Hutson@blackbaud.com>
2018-02-22 11:27:18 -0600

    Remove spring

--- src/main/java/org/whatever/AudioHacking.java
+++ src/main/java/org/whatever/AudioHacking.java
@@ -11,21 +11,16 @@ import javafx.scene.media.MediaView;
 import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 
-@SpringBootApplication
+@Slf4j
 public class AudioHacking extends Application {
-    private static final Logger log = LoggerFactory.getLogger(AudioHacking.class);
 
     private PublishingService publishingService;
 
     public static void main(String[] args) {
-        SpringApplication.run(AudioHacking.class, args);
         launch(args);
         System.exit(0);
     }
